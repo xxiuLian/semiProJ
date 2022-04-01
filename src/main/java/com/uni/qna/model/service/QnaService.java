6e4be46bdd7961205dcfd949b3be641afdf18c1a
Computer <Computer@192.168.219.105>
2022-04-01 20:48:51 +0900

    문의게시판 백 구조생성

--- src/main/java/com/uni/qna/model/service/QnaService.java
+++ src/main/java/com/uni/qna/model/service/QnaService.java
@@ -1,5 +1,33 @@
 package com.uni.qna.model.service;
 
+import static com.uni.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.uni.common.PageInfo;
+import com.uni.qna.model.dao.QnaDao;
+import com.uni.qna.model.dto.Qna;
+
 public class QnaService {
+
+	public ArrayList<Qna> selectList(PageInfo pi) {
+		Connection conn = getConnection();
+		ArrayList<Qna> list = new QnaDao().selectList(conn, pi);
+		
+		close(conn);
+		
+		return list;
+	}
+
+	public int getListCount() {
+		Connection conn = getConnection();
+		
+		int listCount = new QnaDao().getListCount(conn);
+		
+		close(conn);
+		
+		return listCount;
+	}
 
 }
