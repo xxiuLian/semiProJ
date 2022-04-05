a65b44a60d2be17b918ad229707c71d7d7e74143
yonii <yonii@DESKTOP-0SFFJ5Q>
2022-04-05 21:12:34 +0900

    4/5 create myPage / update enrollForm,kakaologin

---
+++ src/main/java/com/uni/member/controller/MyInfoServlet.java
@@ -0,0 +1,58 @@
+package com.uni.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.uni.member.model.dto.Member;
+import com.uni.member.model.service.MemberService;
+
+/**
+ * Servlet implementation class MyInfoServlet
+ */
+@WebServlet("/myInfo.do")
+public class MyInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyInfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Member loginUser = (Member)request.getSession().getAttribute("loginUser");
+		
+		String userId = loginUser.getUserId();
+		
+		Member member = new MemberService().selectMember(userId);
+		RequestDispatcher view = null;
+		if(member != null) {
+			request.setAttribute("loginUser", loginUser);
+			view = request.getRequestDispatcher("views/member/myInfo.jsp");
+		}else {
+			request.setAttribute("msg", "조회 실패하였습니다."); //조회 한 결과 해당 유저 정보가 없을때
+			view = request.getRequestDispatcher("views/common/errorPage.jsp");
+		}
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
