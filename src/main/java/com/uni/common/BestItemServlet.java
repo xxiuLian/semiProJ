99ae1d91d37b2079cfbcdab1795b5d187cb75275
yonii <yonii@DESKTOP-0SFFJ5Q>
2022-04-13 19:56:34 +0900

    update mainpage best상품, update slide img size

---
+++ src/main/java/com/uni/common/BestItemServlet.java
@@ -0,0 +1,49 @@
+package com.uni.common;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.uni.member.model.service.MemberService;
+import com.uni.tgb.model.dto.Tgb;
+
+/**
+ * Servlet implementation class BestItemServlet
+ */
+@WebServlet("")
+public class BestItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BestItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+
+		ArrayList<Tgb> bestItem = new MemberService().bestItem();
+		System.out.println("뮈지");
+		request.setAttribute("bestItem", bestItem);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
