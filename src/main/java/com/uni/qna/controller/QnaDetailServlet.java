d1f06c84415deb3097e5be48e84dcf268948a40e
Computer <Computer@192.168.219.100>
2022-04-16 01:41:23 +0900

    관리자페이지 문의게시판까지 팝업 메뉴바 제거

--- src/main/java/com/uni/qna/controller/QnaDetailServlet.java
+++ src/main/java/com/uni/qna/controller/QnaDetailServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.uni.common.Attachment;
 import com.uni.qna.model.dto.Qna;
 import com.uni.qna.model.service.QnaService;
 
